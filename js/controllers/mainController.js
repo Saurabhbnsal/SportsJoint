c5f867052fe2d5f9572e13d28d5679bc57b26c55
Saurabhbnsal <bansalsaurabh001@gmail.com>
2017-12-19 20:24:36 +0530

    Update mainController.js

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,9 +1,9 @@
 var app = angular.module("myApp", []);
 app.controller('MainController', ['$scope', function($scope){
-  $scope.articles = [ {
+  $scope.largeArticles = [ {
 	link :'#',  
 	cover: 'images/sports-15.jpg',
-	title: 'Lorum Ipsum', 
+	title: 'Lorum Ipsum Lorum Ipsum Lorum Ipsum', 
 	author: 'NAme Here',
 	pubdate: new Date('2014', '03', '08'), 
      
@@ -11,37 +11,45 @@ app.controller('MainController', ['$scope', function($scope){
   {
 	link :'#',  
 	cover: 'images/sports-15.jpg',
-	title: 'Lorum Ipsum', 
-	author: 'NAme Here',
-	pubdate: new Date('2014', '03', '08'), 
-     
-  },{
-	link :'#',  
-	cover: 'images/sports-15.jpg',
-	title: 'Lorum Ipsum', 
-	author: 'NAme Here',
-	pubdate: new Date('2014', '03', '08'), 
-     
-  },{
-	link :'#',  
-	cover: 'images/sports-15.jpg',
-	title: 'Lorum Ipsum', 
-	author: 'NAme Here',
-	pubdate: new Date('2014', '03', '08'), 
-     
-  },{
-	link :'#',  
-	cover: 'images/sports-15.jpg',
-	title: 'Lorum Ipsum', 
-	author: 'NAme Here',
-	pubdate: new Date('2014', '03', '08'), 
-  },{
-	link :'#',  
-	cover: 'images/sports-15.jpg',
-	title: 'Lorum Ipsum', 
+	title: 'Lorum Ipsum Lorum Ipsum Lorum Ipsum', 
 	author: 'NAme Here',
 	pubdate: new Date('2014', '03', '08'), 
      
   }
+  ];
+  
+  $scope.smallArticles = [ {
+	link :'#',  
+	cover: 'images/sports-15.jpg',
+	title: 'Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum' , 
+	author: 'NAme Here',
+	pubdate: new Date('2014', '03', '08'), 
+     
+  },
+  {
+	link :'#',  
+	cover: 'images/sports-15.jpg',
+	title: 'Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum', 
+	author: 'NAme Here',
+	pubdate: new Date('2014', '03', '08'), 
+     
+  },
+  {
+	link :'#',  
+	cover: 'images/sports-15.jpg',
+	title: 'Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum', 
+	author: 'NAme Here',
+	pubdate: new Date('2014', '03', '08'), 
+     
+  },
+  {
+	link :'#',  
+	cover: 'images/sports-15.jpg',
+	title: 'Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum', 
+	author: 'NAme Here',
+	pubdate: new Date('2014', '03', '08'), 
+     
+  }
+  
   ];
 }]);
